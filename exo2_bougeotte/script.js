08cadb1caf592725a2ff6ceb1fad287d88859e9f
Diarapak <krobpierre0@gmail.com>
2025-03-19 10:51:42 +0100

    exos2

---
+++ exo2_bougeotte/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('bougeotte');
+
+button.addEventListener('mouseover', () => {
+    const x = Math.random() * (window.innerWidth - button.offsetWidth);
+    const y = Math.random() * (window.innerHeight - button.offsetHeight);
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+});
