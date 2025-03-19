c41e0c0e5963f8abe528da39ca65f55710eea4b9
PierreKROB <krobpierre0@gmail.com>
2025-03-19 09:22:43 +0100

    exos1

--- exo1_composantCopy/script.js
+++ exo1_composantCopy/script.js
@@ -1,1 +1,9 @@
-test
+function CopyComponent(id) {
+    var text = document.getElementById(id).innerText;
+
+    navigator.clipboard.writeText(text).then(function() {
+        alert("Texte copié");
+    }).catch(function(err) {
+        console.error('Erreur lors de la copie du texte: ', err);
+    });
+}
