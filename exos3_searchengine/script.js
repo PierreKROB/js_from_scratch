939ca0340d0b68f3d8e92eac465f66a2f3d72dde
Diarapak <krobpierre0@gmail.com>
2025-04-14 10:43:52 +0200

    fix

--- exos3_searchengine/script.js
+++ exos3_searchengine/script.js
@@ -21,30 +21,30 @@ class SearchEngine {
 document.addEventListener('DOMContentLoaded', () => { //uselesss cause defer on html
     const searchEngine = new SearchEngine();
     const searchInput = document.getElementById('searchInput');
-    const divResultats = document.getElementById('results');
+    const divResults = document.getElementById('results');
 
-    const tempo = (fonction, delai) => {
+    const tempo = (fonction, delay) => {
         let minuteur;
         return (...args) => {
             clearTimeout(minuteur);
-            minuteur = setTimeout(() => fonction.apply(this, args), delai);
+            minuteur = setTimeout(() => fonction.apply(this, args), delay);
         };
     };
 
-    const gererSaisie = async (evenement) => {
-        const requete = evenement.target.value;
-        if (requete.length >= 3) {
-            const resultats = await searchEngine.search(requete);
+    const handleInput = async (evenement) => {
+        const request = evenement.target.value;
+        if (request.length >= 3) {
+            const resultats = await searchEngine.search(request);
             if (resultats && resultats.features) {
-                const etiquettes = resultats.features.map(element => element.properties.label);
-                divResultats.innerHTML = etiquettes.join('<br>');
+                const answer = resultats.features.map(element => element.properties.label);
+                divResults.innerHTML = answer.join('<br>');
             } else {
-                divResultats.innerHTML = 'Aucun résultat trouvé.';
+                divResults.innerHTML = 'Aucun résultat trouvé.';
             }
         } else {
-            divResultats.innerHTML = '';
+            divResults.innerHTML = '';
         }
     };
 
-    searchInput.addEventListener('input', tempo(gererSaisie, 300));
+    searchInput.addEventListener('input', tempo(handleInput, 300));
 });
